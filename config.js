30f182c841529aa2616ac0b43504c1686c47eaf4
nguyen6505 <nguyennguyen672004@gmail.com>
2024-08-02 22:10:42 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Chúc mừng sinh nhật",
+    introTitle: 'Thư hay CHÊ ',
+    introDesc: `Chúc Thư Sinh nhật dui dẻ ha`,
+    btnIntro: 'Muốn cảm ơn thì nhấn vào đây',
+    title: 'Huy đẹp chai gê thư ha ???-_-',
+    desc: 'Thư nhấn "Đúng zậy" đi',
+    btnYes: 'Đúng zậy <33',
+    btnNo: 'Không nha :3',
+    question: 'Sao thư hay chê quá z <3',
+    btnReply: 'Gửi cho huy <3',
+    reply: 'Nói cho vui thôi chớ không phải sự thật <3',
+    mess: 'Huy biết mà >_< . hê hê hê',
+    messDesc: 'Hẹn Thư chìu mai ăn bánh kem nha...',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100038229586347' //gắn link facebook vào đây 
+}
